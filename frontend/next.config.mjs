f4c71a52bba49c68550a4a2687d02378e7363185
Kevin Torregrosa <142925854+MrManini@users.noreply.github.com>
2026-05-16 10:01:58 -0500

    Add environment variable for backend URL

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
